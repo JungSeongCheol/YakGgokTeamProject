2b52e3262816482a9fbaa9484f55730daf1b6050
채은 이 <am1752@outlook.kr>
2020-09-04 10:26:03 +0900

    PharmApp

---
+++ PharmApp/LoginApp/ViewModels/ResetViewModel.cs
@@ -0,0 +1,116 @@
+﻿using LoginApp.Models;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LoginApp.ViewModels
+{
+    public class ResetViewModel : BindableBase
+    {
+        IRegionManager _regionManager;
+
+        
+        public DelegateCommand<string> ResetCommand { get; set; }
+
+        string strConnString = "Server=localhost;Port=3306;" +
+                               "Database=gguklist;Uid=root;Pwd=mysql_p@ssw0rd;";
+
+        int Id = Commons.Dev_Id;
+
+        private string pwd;
+        public string Pwd
+        {
+            get => pwd;
+            set => SetProperty(ref pwd, value);
+        }
+
+        private string ckPwd;
+
+        public string CkPwd
+        {
+            get => ckPwd;
+            set => SetProperty(ref ckPwd, value);
+        }
+
+
+        public ResetViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            ResetCommand = new DelegateCommand<string>(Reset);
+        }
+
+        private void Reset(string uri)
+        {
+            if(Pwd != CkPwd)
+            {
+                MessageBox.Show("비밀번호를 다시 입력해주세요");
+                return;
+            }
+
+            else
+            {
+                string strQuery = " UPDATE plist " +
+                               " SET " +
+                               " PW = @Pwd, " +
+                               " CK = 1" +
+                               " WHERE " +
+                               " DEV_ID = @Id ";
+
+                using (MySqlConnection conn = new MySqlConnection(strConnString))
+                {
+
+                    conn.Open();
+                    //MetroMessageBox.Show(this, $"DB접속성공!!");
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strQuery;
+                    MySqlParameter paramPwd = new MySqlParameter("@Pwd", MySqlDbType.VarChar);
+                    string PW = Pwd;
+                    var md5Hash = MD5.Create();
+                    var cryptoPassword = Commons.GetMd5Hash(md5Hash, Pwd);
+                    paramPwd.Value = cryptoPassword;
+
+                    MySqlParameter paramid = new MySqlParameter("@Id", MySqlDbType.Int32);
+                    paramid.Value = Id;
+
+                    cmd.Parameters.Add(paramid);
+                    cmd.Parameters.Add(paramPwd);
+                    cmd.ExecuteNonQuery();
+                }
+
+                _regionManager.RequestNavigate("ContentRegion", uri);
+                _regionManager.Regions["Find"].RemoveAll();
+                _regionManager.Regions["Reset"].RemoveAll();
+                _regionManager.Regions["Menu"].RemoveAll();
+            }
+        }
+
+        private void upCK()
+        {
+            string strQuery = " UPDATE plist " +
+                              " SET " +
+                              " PW = @Pwd " +
+                              " WHERE " +
+                              " DEV_ID = @Id ";
+
+            using (MySqlConnection conn = new MySqlConnection(strConnString))
+            {
+
+                conn.Open();
+                //MetroMessageBox.Show(this, $"DB접속성공!!");
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = strQuery;
+            }
+        }
+    }
+}
---
+++ PharmApp/ProcessApp/ViewModels/MenuViewModel.cs
@@ -0,0 +1,301 @@
+﻿using LoginApp.Models;
+using MySql.Data.MySqlClient;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+using ProcessApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProcessApp.ViewModels 
+{
+    public class MenuViewModel : BindableBase
+    {
+        public DelegateCommand OutPutCommand { get; set; }
+        public DelegateCommand<string> ControlCommand { get; set; }
+        public DelegateCommand ResetsCommand { get; set; }
+        public DelegateCommand<string> LoginCommand { get; set; }
+        public DelegateCommand<string> InfoCommand { get; set; }
+        private IRegionManager _regionManager;
+
+        List<PrescriptionModel> psList;
+        public List<PrescriptionModel> PsList
+        {
+            get => psList;
+            set => SetProperty(ref psList, value);
+        }
+
+
+        private string hosNum = "";
+        public string HosNum
+        {
+            get => hosNum;
+            set => SetProperty(ref hosNum, value);
+        }
+        private string name = null;
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private string medicine = null;
+        public string Medicine
+        {
+            get => medicine;
+            set => SetProperty(ref medicine, value);
+        }
+
+        private string date = null;
+
+        public string Datee
+        {
+            get => date;
+            set => SetProperty(ref date, value);
+        }
+
+        //private bool v = false;
+
+        //public bool V
+        //{
+        //    get => v;
+        //    set => SetProperty(ref v, value);
+        //}
+
+        private string[] mList = {"","A","B","C","D" };
+
+        public string[] MList
+        {
+            get => mList;
+            set => SetProperty(ref mList, value);
+        }
+
+        string strConnString = "Server=localhost;Port=3306;" +
+                               "Database=hospital;Uid=root;Pwd=mysql_p@ssw0rd;";
+
+
+        public MenuViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            
+            OutPutCommand = new DelegateCommand(Navigate);
+            ControlCommand = new DelegateCommand<string>(Controls);
+            ResetsCommand = new DelegateCommand(Resetting);
+            LoginCommand = new DelegateCommand<string>(LogOut);
+            InfoCommand = new DelegateCommand<string>(Infos);
+        }
+
+        private void Infos(string uri)
+        {
+            _regionManager.RequestNavigate("Hello", uri);
+
+            _regionManager.Regions["Menu"].RemoveAll();
+        }
+
+        private void LogOut(string uri)
+        {
+            _regionManager.RequestNavigate("ContentRegion", uri);
+
+            _regionManager.Regions["Menu"].RemoveAll();
+            _regionManager.Regions["Hello"].RemoveAll();
+        }
+
+        private void Controls(string uri)
+        {
+            _regionManager.RequestNavigate("Hello", uri);
+
+            _regionManager.Regions["Menu"].RemoveAll();
+        }
+
+        private void Resetting()
+        {
+            Name = Datee = null;
+            Medicine = MList[0];
+        }
+
+        private void Navigate()
+        {
+            //SendPara();
+            PsList = new List<PrescriptionModel>();
+            SelectItem();
+
+            
+        }
+
+
+        private void SelectItem()
+        {
+            string strQuery = " SELECT * " + " FROM prescription " +
+                              " Where ";
+            string _Hos = " HospitalNum = @HospitalNum ";
+            string _MM = "  Medicine = @Medicine ";
+            string _Datee = " Date = @Datee ";
+            string _Name = " Name = @Name ";
+
+            if (string.IsNullOrEmpty(Datee) && string.IsNullOrEmpty(Medicine) &&
+               string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(HosNum))
+            {
+
+            }
+
+
+            else if (string.IsNullOrEmpty(Datee) && string.IsNullOrEmpty(Medicine) && string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _Name);
+            }
+
+            else if (string.IsNullOrEmpty(Datee) && string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _MM);
+            }
+
+            else if (string.IsNullOrEmpty(Medicine) && string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _Datee);
+            }
+
+            else if (string.IsNullOrEmpty(Medicine) && string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(_Datee))
+            {
+                strQuery += ("AND" + _Hos);
+            }
+
+            else if (string.IsNullOrEmpty(Datee) && string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _Name + "AND" + _MM);
+            }
+
+            else if (string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _MM + "AND" + _Datee);
+            }
+
+            else if (string.IsNullOrEmpty(Medicine) && string.IsNullOrEmpty(Datee))
+            {
+                strQuery += ("AND" + _Name + "AND" + _Hos);
+            }
+
+            else if (string.IsNullOrEmpty(Datee) && string.IsNullOrEmpty(Medicine))
+            {
+                strQuery += ("AND" + _Name + "AND" + _Hos);
+            }
+
+            else if (string.IsNullOrEmpty(Name) && string.IsNullOrEmpty(Datee))
+            {
+                strQuery += ("AND" + _MM + "AND" + _Hos);
+            }
+
+            else if (string.IsNullOrEmpty(Medicine) && string.IsNullOrEmpty(Name))
+            {
+                strQuery += ("AND" + _Datee + "AND" + _Hos);
+            }
+
+            else if (string.IsNullOrEmpty(HosNum))
+            {
+                strQuery += ("AND" + _Name + "AND" + _Hos + "AND" + _MM);
+            }
+
+            else if (string.IsNullOrEmpty(Datee))
+            {
+                strQuery += ("AND" + _Name + "AND" + _Hos + "AND" + _MM);
+            }
+
+            else if (string.IsNullOrEmpty(Name))
+            {
+                strQuery += ("AND" + _MM + "AND" + _Hos + "AND" + _Datee);
+            }
+
+            else if (string.IsNullOrEmpty(Medicine))
+            {
+                strQuery += ("AND" + _Datee + "AND" + _Hos + "AND" + _MM);
+            }
+
+            else
+            {
+                strQuery += (_Datee + _Name + _MM + _Hos);
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(strConnString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strQuery;
+
+                    if (!string.IsNullOrEmpty(Name))
+                    {
+                        MySqlParameter paramHospitalNum = new MySqlParameter("@HospitalNum", MySqlDbType.VarChar);
+                        paramHospitalNum.Value = Commons.Dev_Id;
+                        cmd.Parameters.Add(paramHospitalNum);
+
+                    }
+                        
+
+
+                    if (!string.IsNullOrEmpty(Name))
+                    {
+                        MySqlParameter paramName = new MySqlParameter("@Name", MySqlDbType.VarChar);
+                        paramName.Value = Name;
+                        cmd.Parameters.Add(paramName);
+                    }
+
+                    if (!string.IsNullOrEmpty(Medicine))
+                    {
+                        MySqlParameter paramMedicine = new MySqlParameter("@Medicine", MySqlDbType.VarChar);
+                        paramMedicine.Value = Medicine;
+                        cmd.Parameters.Add(paramMedicine);
+                    }
+
+                    if (!string.IsNullOrEmpty(HosNum))
+                    {
+                        MySqlParameter paramHos = new MySqlParameter("@HospitalNum", MySqlDbType.VarChar);
+                        paramHos.Value = HosNum;
+                        cmd.Parameters.Add(paramHos);
+                    }
+
+                    if (!string.IsNullOrEmpty(Datee))
+                    {
+                        MySqlParameter paramDatee = new MySqlParameter("@Datee", MySqlDbType.VarChar);
+                        paramDatee.Value = Datee;
+                        cmd.Parameters.Add(paramDatee);
+                    }
+
+          
+                    //MySqlDataAdapter adapter = new MySqlDataAdapter(strQuery, conn);
+
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        var temp = new PrescriptionModel
+                        {
+                            PreNum = int.Parse(reader["PreNum"].ToString()),
+                            HospitalNum = int.Parse(reader["HospitalNum"].ToString()),
+                            Name = reader["Name"].ToString(),
+                            Date = reader["Date"].ToString(),
+                            Medicine = reader["Medicine"].ToString()
+                        };
+                        PsList.Add(temp);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+    }
+}
